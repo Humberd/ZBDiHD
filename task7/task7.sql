669d86cd44903922251350de1f9450a3138dff29
Humberd <humberd2@gmail.com>
2018-04-11 17:31:33 +0200

    Task:7 done task 5

--- task7/task7.sql
+++ task7/task7.sql
@@ -37,22 +37,42 @@ FROM PART P
 GROUP BY GROUPING SETS ((C.CITY, P.PART#, P.NAME), (C.CITY), ());
 
 --zad 4
-SELECT PART#, NAME, CITY,  total_purchase
+SELECT
+  PART#,
+  NAME,
+  CITY,
+  total_purchase
 FROM (
   SELECT
     P.PART#,
     P.NAME,
     C.CITY,
-    SUM(D.NUM_ORDERED) as total_purchase,
+    SUM(D.NUM_ORDERED) AS total_purchase,
     RANK()
     OVER (
       PARTITION BY P.PART#
       ORDER BY SUM(D.NUM_ORDERED) DESC
-      ) as Rank
+      )                AS Rank
   FROM PART P
     JOIN ORDER_DETAILS D ON P.PART# = D.PART#
     JOIN T_ORDERS O ON O.ORDER# = D.ORDER#
     JOIN CUSTOMER C ON C.CUSID = O.CUSID
   GROUP BY GROUPING SETS ((C.CITY, P.PART#, P.NAME)), P.PART#
 )
-WHERE Rank = 1
+WHERE Rank = 1;
+
+--zad 5
+SELECT
+  c.CUSID,
+  c.cus_name,
+  nvl(count(DISTINCT O.ORDER#), 0)            AS num_order,
+  nvl(count(P.PART#), 0)                      AS ind_pasrts,
+  TO_CHAR(nvl(sum(D.QUOTED_PRICE * D.NUM_ORDERED), 0), 'L99G999D99MI') AS total
+FROM PART P
+  JOIN ORDER_DETAILS D ON P.PART# = D.PART#
+  JOIN T_ORDERS O ON O.ORDER# = D.ORDER#
+  RIGHT JOIN CUSTOMER C ON C.CUSID = O.CUSID
+GROUP BY GROUPING SETS ((c.CUSID, c.cus_name))
+ORDER BY total DESC
+
+--zad 6
