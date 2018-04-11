ed646cb26cc1e1f69b951015163d56ceefb0b75b
Humberd <humberd2@gmail.com>
2018-04-11 16:58:45 +0200

    Task:7 done task 2

--- task7/task7.sql
+++ task7/task7.sql
@@ -1,12 +1,25 @@
 --zad 1
 SELECT
-  P.P_NUM || ': ' || P.NAME                     AS Part,
-  COUNT(D.ORDER#)                               AS number_Of_Orders,
-  TO_CHAR(NVL(AVG(D.NUM_ORDERED * D.QUOTED_PRICE), 0),'L99G999D99MI') AS Sum,
+  P.P_NUM || ': ' || P.NAME                                               AS Part,
+  COUNT(D.ORDER#)                                                         AS number_Of_Orders,
+  TO_CHAR(NVL(AVG(D.NUM_ORDERED * D.QUOTED_PRICE), 0), 'L99G999D99MI')    AS Sum,
   RANK()
-  OVER(ORDER BY ROUND(NVL(AVG(D.NUM_ORDERED * D.QUOTED_PRICE), 0), 2) DESC) as Ranking
+  OVER (
+    ORDER BY ROUND(NVL(AVG(D.NUM_ORDERED * D.QUOTED_PRICE), 0), 2) DESC ) AS Ranking
 FROM PART P
   LEFT JOIN ORDER_DETAILS D ON P.PART# = D.PART#
-GROUP BY P.P_NUM, P.NAME
+GROUP BY P.P_NUM, P.NAME;
 
 --zad 2
+SELECT *
+FROM (SELECT
+        P.P_NUM || ': ' || P.NAME                    AS Part,
+        COUNT(D.ORDER#)                              AS number_Of_Orders,
+        SUM(NVL(D.NUM_ORDERED, 0))                   AS quantity_ordered,
+        RANK()
+        OVER (
+          ORDER BY SUM(NVL(D.NUM_ORDERED, 0)) DESC ) AS Ranking
+      FROM PART P
+        LEFT JOIN ORDER_DETAILS D ON P.PART# = D.PART#
+      GROUP BY P.P_NUM, P.NAME)
+WHERE ROWNUM < 4;
