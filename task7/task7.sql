6337f374b4fb9d811578bd12618151c792765ec3
Humberd <humberd2@gmail.com>
2018-04-11 21:30:51 +0200

    Task:7 done task 7

--- task7/task7.sql
+++ task7/task7.sql
@@ -76,12 +76,11 @@ GROUP BY GROUPING SETS ((c.CUSID, c.cus_name))
 ORDER BY total DESC;
 
 --zad 6
--- SELECT CUSID, CUS_NAME, CREDIT_LIMIT, available_credit
 SELECT
   CUSID,
   CUS_NAME,
-  TO_CHAR(CREDIT_LIMIT, 'L99G999D99MI') as CREDIT_LIMIT,
-  TO_CHAR(available_credit, 'L99G999D99MI') as available_credit
+  TO_CHAR(CREDIT_LIMIT, 'L99G999D99MI')     AS CREDIT_LIMIT,
+  TO_CHAR(available_credit, 'L99G999D99MI') AS available_credit
 FROM (
   SELECT
     CUSID,
@@ -113,6 +112,64 @@ FROM (
   )
   ORDER BY RANK ASC, CREDIT_LIMIT DESC
 )
-WHERE Rank = 1
+WHERE Rank = 1;
 
--- WHERE Rank = 1
+--zad 7
+SELECT
+  name,
+  Total,
+  Indicator
+FROM (
+  SELECT
+    R.REPID || ': ' || trim(R.LAST_NAME) || ', ' || trim(R.FIRST_NAME) AS name,
+    TO_CHAR(SUM(D.NUM_ORDERED * D.QUOTED_PRICE), 'L99G999D99MI')       AS Total,
+    'Total Value of Sales'                                             AS Indicator,
+    RANK()
+    OVER (
+      ORDER BY SUM(D.NUM_ORDERED * D.QUOTED_PRICE) DESC
+      )                                                                AS Rank
+  FROM REP R
+    JOIN T_ORDERS ORDER2 ON R.REPID = ORDER2.REPID
+    JOIN ORDER_DETAILS D ON ORDER2.ORDER# = D.ORDER#
+  GROUP BY R.REPID, R.LAST_NAME, R.FIRST_NAME
+)
+WHERE Rank = 1
+UNION
+SELECT
+  name,
+  Total,
+  Indicator
+FROM (
+  SELECT
+    R.REPID || ': ' || trim(R.LAST_NAME) || ', ' || trim(R.FIRST_NAME) AS name,
+    to_char(SUM(D.NUM_ORDERED))                                        AS Total,
+    'Quantity of items sold'                                           AS Indicator,
+    RANK()
+    OVER (
+      ORDER BY SUM(NUM_ORDERED) DESC
+      )                                                                AS Rank
+  FROM REP R
+    JOIN T_ORDERS ORDER2 ON R.REPID = ORDER2.REPID
+    JOIN ORDER_DETAILS D ON ORDER2.ORDER# = D.ORDER#
+  GROUP BY R.REPID, R.LAST_NAME, R.FIRST_NAME
+)
+WHERE Rank = 1
+UNION
+SELECT
+  name,
+  Total,
+  Indicator
+FROM (
+  SELECT
+    R.REPID || ': ' || trim(R.LAST_NAME) || ', ' || trim(R.FIRST_NAME) AS name,
+    TO_CHAR(COUNT(ORDER2.ORDER#))                                      AS Total,
+    'Total number of orders'                                           AS Indicator,
+    RANK()
+    OVER (
+      ORDER BY COUNT(ORDER2.ORDER#) DESC
+      )                                                                AS Rank
+  FROM REP R
+    JOIN T_ORDERS ORDER2 ON R.REPID = ORDER2.REPID
+  GROUP BY GROUPING SETS ((R.REPID, R.LAST_NAME, R.FIRST_NAME))
+)
+WHERE Rank = 1;
